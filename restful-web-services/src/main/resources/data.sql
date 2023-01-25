ea39c0850afe266cf4c3c43f3e652e73b0426246
Sohrab Alam <sohrab.alam86@gmail.com>
2023-01-25 19:15:24 +0530

    30. SQL Server DB connected to Spring Boot Project

--- restful-web-services/src/main/resources/data.sql
+++ restful-web-services/src/main/resources/data.sql
@@ -1,6 +1,8 @@
-INSERT INTO user_details(id, birth_date, name)VALUES(10001, current_date(), 'Alizabeth');
-INSERT INTO user_details(id, birth_date, name)VALUES(10002, current_date(), 'Celina');
-INSERT INTO user_details(id, birth_date, name)VALUES(10003, current_date(), 'David');
+-- GETDATE() is current date function for SQL Server Database. 
+-- But Current_Date() function was used for H2 in-memory database.
+INSERT INTO user_details(id, birth_date, name)VALUES(10001, GETDATE(), 'Alizabeth');
+INSERT INTO user_details(id, birth_date, name)VALUES(10002, GETDATE(), 'Celina');
+INSERT INTO user_details(id, birth_date, name)VALUES(10003, GETDATE(), 'David');
 
 INSERT INTO designation(designation_id, description, user_details_id)
 VALUES(20001, 'I am working on Spring Boot', 10001);
@@ -10,6 +12,3 @@ VALUES(20002, 'I am working on Microservices', 10001);
 
 INSERT INTO designation(designation_id, description, user_details_id)
 VALUES(20003, 'I am working on AWS', 10002);
-
-INSERT INTO designation(designation_id, description, user_details_id)
-VALUES(20004, 'I am working on DevOps', 10002);
